455538f4a6a75eea647d801b8a74e17c8c666a5e
Richie Bachtiar <richiebachtiar@gmail.com>
2016-12-02 21:11:56 +0700

    Initial commit

---
+++ app/src/main/java/com/runexa/hci_uas/HomeFragment.java
@@ -0,0 +1,47 @@
+package com.runexa.hci_uas;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        GridView gv = (GridView) v.findViewById(R.id.home_gridview);
+        gv.setAdapter(new HomeFragmentAdapter(this.getContext()));
+
+        return v;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
